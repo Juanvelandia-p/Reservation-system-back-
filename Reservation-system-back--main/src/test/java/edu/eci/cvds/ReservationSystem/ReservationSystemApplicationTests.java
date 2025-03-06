adc63dbc47c5e47c9e5713c80ee7ef51cd667c53
unknown <daniel.ruge-g@labinfo.is.escuelaing.edu.co>
2025-03-06 13:53:21 -0500

    organizacion MVC

---
+++ Reservation-system-back--main/src/test/java/edu/eci/cvds/ReservationSystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,9 @@
+package edu.eci.cvds.ReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+}
